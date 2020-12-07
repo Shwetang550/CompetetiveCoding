d440e720087687855386077f8bc922dd2b04d416
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-08 02:18:20 +0530

    Life, the Universe, and Everything

---
+++ HackerEarth/Life-Universe-Everything.cpp
@@ -0,0 +1,22 @@
+/*Your program is to use the brute-force approach in order to find the Answer 
+to Life, the Universe, and Everything. More precisely... rewrite small numbers 
+from input to output. Stop processing input after reading in the number 42.  
+ All numbers at input are integers of one or two digits.*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n != 42)
+    {
+        cout << n << endl;
+        cin >> n;
+    }
+
+    return 0;
+}
