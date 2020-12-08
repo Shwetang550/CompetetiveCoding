1246300e2b830df726ceda95ec1cbfeeb7321bc3
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-08 19:29:31 +0530

    Hello

---
+++ HackerEarth/Hello.cpp
@@ -0,0 +1,12 @@
+/*Kirti is new to programming and she doesn't know how to code. Can you write a code, 
+which would print "Hello Kirti" on the screen.*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Kirti";
+    return 0;
+}
