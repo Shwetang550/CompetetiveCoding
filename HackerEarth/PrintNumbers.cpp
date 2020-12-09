2b40fccdc513b754c4ef0256f49a92fe2790ece6
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-10 00:04:32 +0530

    Print Numbers

---
+++ HackerEarth/PrintNumbers.cpp
@@ -0,0 +1,19 @@
+/*Given N numbers in the input. Print these numbers in the same order as they come in input.*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+        cout << n << " ";
+    }
+
+    return 0;
+}
