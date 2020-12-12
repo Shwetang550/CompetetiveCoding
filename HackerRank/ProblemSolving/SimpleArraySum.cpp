97f9b25d8b95cb74e7d12def6dc2bc25c7190e89
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-12 12:28:25 +0530

    Simple Array Sum

---
+++ HackerRank/ProblemSolving/SimpleArraySum.cpp
@@ -0,0 +1,20 @@
+/*Given an array of integers, find the sum of its elements.*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n], sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    cout << sum;
+    return 0;
+}
