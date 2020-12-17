64a6535c10d356aa8675e1b43848812150bd8b14
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-17 22:56:49 +0530

    Divisible Sum Pair

---
+++ HackerRank/ProblemSolving/DivisibleSumPair.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    int a[n],c=0;
+
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if((a[i]+a[j])%k==0)
+            c++;
+        }
+    }
+
+    cout<<c;
+    return 0;
+}
