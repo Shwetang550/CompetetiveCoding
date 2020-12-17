0bf6c12e53ab45a7b5478518da37bdd045659ecb
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-17 23:05:51 +0530

    Divisible Sum Pair

--- HackerRank/ProblemSolving/DivisibleSumPair.cpp
+++ HackerRank/ProblemSolving/DivisibleSumPair.cpp
@@ -1,3 +1,6 @@
+/*You are given an array of n integers, ar=[ar[0],ar[1],...,ar[n-1]], and a positive integer, k. 
+ Find and print the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible by k.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
