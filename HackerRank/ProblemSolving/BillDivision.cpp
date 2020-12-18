da86efe6364e33d7da9e54cb71e331532e572a54
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-18 22:45:15 +0530

    Bill Division (Problem Statement added)

--- HackerRank/ProblemSolving/BillDivision.cpp
+++ HackerRank/ProblemSolving/BillDivision.cpp
@@ -1,3 +1,7 @@
+/*Two friends Anna and Brian, are deciding how to split the bill at a dinner. 
+Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. 
+You must determine if his calculation is correct.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
