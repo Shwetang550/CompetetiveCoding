d73878acb2649d38e101e0a9dfab8143e0463ee9
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-23 21:10:48 +0530

    Picking Numbers (Problem Statement added)

--- HackerRank/ProblemSolving/PickingNumbers.cpp
+++ HackerRank/ProblemSolving/PickingNumbers.cpp
@@ -1,3 +1,6 @@
+/*Given an array of integers, find the longest subarray where the absolute 
+difference between any two elements is less than or equal to 1.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
