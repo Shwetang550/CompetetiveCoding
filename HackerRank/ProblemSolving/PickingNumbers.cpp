b96f308f5e25396508dba4f2d23a1a11f8bb09ca
Shwetang <shwetangkumarsingh@gmail.com>
2020-12-23 21:06:07 +0530

    Picking Numbers

---
+++ HackerRank/ProblemSolving/PickingNumbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,s=0,mx=0,sum=0,a[101],c[99]={0};
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+        c[a[i]]++;
+    }
+
+    for(int i=1; i<100; i++)
+    {
+        if(mx<c[i] && sum<(c[i]+max(c[i-1],c[i+1])))
+        {
+            mx=c[i];
+            s=max(c[i-1],c[i+1]);
+            sum=mx+s;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
