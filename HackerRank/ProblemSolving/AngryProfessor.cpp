d9fa017441714053805ae9c8e281a555cf6fbf84
Shwetang <shwetangsingh5@gmail.com>
2021-01-09 23:06:25 +0530

    Angry Professor

---
+++ HackerRank/ProblemSolving/AngryProfessor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+            if(a[i]<=0)
+            k--;
+        }
+        if(k<=0)
+        cout<<"NO\n";
+        else
+        cout<<"YES\n";
+    }
+    return 0;
+}
