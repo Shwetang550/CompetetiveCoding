7a8fd3ef57e425fd4bf67166bb14841600e5ebc5
Shwetang <shwetangsingh5@gmail.com>
2021-01-10 22:34:39 +0530

    Beautiful Days at the Movies

---
+++ HackerRank/ProblemSolving/BeautifulDaysAtMovies.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,k,c=0;
+    cin>>x>>y>>k;
+    for(int i=x; i<=y; i++)
+    {
+        int n=i,rev=0;
+        while(n>0)
+        {
+            int f;
+            f=n%10;
+            rev=rev*10+f;
+            n=n/10;
+        }
+        if((i-rev)%k==0)
+        c++;
+    }
+    cout<<c;
+    return 0;
+}
