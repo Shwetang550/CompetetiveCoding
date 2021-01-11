7940122ccdccaab1b2e86eb9b5220eed1e18c6a2
Shwetang <shwetangsingh5@gmail.com>
2021-01-11 21:17:37 +0530

    Save the Prisoner

---
+++ HackerRank/ProblemSolving/SaveThePrisoner.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        int n,m,s;
+        cin>>n>>m>>s;
+        --s;
+        s+=m;
+        if(s>n)
+        s=s%n;
+        if(s==0)
+        s=n;
+        cout<<s<<endl;
+    }
+    return 0;
+}
